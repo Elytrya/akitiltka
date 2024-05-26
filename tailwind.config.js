e1a121e7d58790f5e1f31e0169172938a0c2670d
Elytrya <150997697+Elytrya@users.noreply.github.com>
2024-05-26 11:27:36 +0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["*.html"],
+  darkMode: ['selector', '[data-mode="dark"]'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
